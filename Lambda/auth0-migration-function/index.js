b6d0de0712f80720a4c157f5fef21a9762c8abde
Matthew Folker <matthew.folker@sage.com>
2019-05-17 14:44:48 +0100

    Auto stash before merge of "master" and "origin/master"

--- Lambda/auth0-migration-function/index.js
+++ Lambda/auth0-migration-function/index.js
@@ -1,8 +1,81 @@
-exports.handler = async event => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda! - Test Monkey")
-  };
-  return response;
+"use strict";
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  console.log(JSON.stringify(event));
+  console.log(JSON.stringify(context));
+
+  try {
+    var params = {
+      TableName: "MigrantUsers",
+      ProjectionExpression: "emailAddress, basicAuth.password",
+      FilterExpression: "emailAddress = " + "employer.publisher@sage.com"
+    };
+
+    const bcrypt = require("bcrypt");
+
+    let statusCode = 500;
+    let message = "Something is missing - pipeline test";
+
+    console.log("about to scan");
+
+    // await docClient.scan(params, onScan);
+
+    // console.log("scan complete");
+
+    // function onScan(err, data) {
+    //   if (err) {
+    //     console.error(
+    //       "Unable to scan the table. Error JSON:",
+    //       JSON.stringify(err, null, 2)
+    //     );
+    //   } else {
+    //     console.log("Scan succeeded.");
+
+    //     if (!data.Items || data.Items[0].length < 1) {
+    //       statusCode = 404;
+    //       message = "Too many or too few";
+    //       return;
+    //     }
+
+    //     statusCode = 200;
+    //     message = "user matched";
+    //   }
+    // }
+
+    switch (event.path) {
+      case "/login":
+        return {
+          statusCode: statusCode,
+          body: message
+        };
+        break;
+
+      case "/get-user":
+        return {
+          statusCode: 200,
+          body: "Get user"
+        };
+        break;
+
+      default:
+        console.error(
+          "Request to unknown end point received. Event details:",
+          JSON.stringify(event)
+        );
+        return {
+          statusCode: 404,
+          body: JSON.stringify(event)
+        };
+    }
+
+    return response;
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 200,
+      body: e
+    };
+  }
 };
