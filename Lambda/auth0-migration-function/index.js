406ffb96d6e57b3ed9614aef92b547c9bb4d80b5
Matthew Folker <matthew.folker@sage.com>
2019-05-17 16:54:12 +0100

    Change to await promise

--- Lambda/auth0-migration-function/index.js
+++ Lambda/auth0-migration-function/index.js
@@ -6,91 +6,33 @@ const bcrypt = require("bcrypt");
 exports.handler = async event => {
   console.log(JSON.stringify(event));
 
+  var params = {
+    TableName: "MigrantUsers",
+    ProjectionExpression: "emailAddress, basicAuth.password",
+    ExpressionAttributeNames: {
+      "#emailAddress": "emailAddress"
+    },
+    ExpressionAttributeValues: {
+      ":emailAddress": "employer.publisher@sage.com"
+    },
+    FilterExpression: "#emailAddress = :emailAddress"
+  };
+
+  let statusCode = 500;
+  let responseBody = null;
+
   try {
-    var params = {
-      TableName: "MigrantUsers",
-      ProjectionExpression: "emailAddress, basicAuth.password",
-      ExpressionAttributeNames: {
-        "#emailAddress": "emailAddress"
-      },
-      ExpressionAttributeValues: {
-        ":emailAddress": "employer.publisher@sage.com"
-      },
-      FilterExpression: "#emailAddress = :emailAddress"
-    };
-
-    let statusCode = 500;
-    let message = "Something is missing - pipeline test - auto test";
-
-    console.log("about to scan");
-
-    await docClient.scan(params, onScan);
-
-    // console.log("scan complete");
-
-    let result = null;
-
-    function onScan(err, data) {
-      console.log("on scan");
-
-      if (err) {
-        console.error(
-          "Unable to scan the table. Error JSON:",
-          JSON.stringify(err)
-        );
-        return;
-      } else {
-        console.log("Scan succeeded.");
-
-        // if (!data.items || data.items[0].length < 1) {
-        //   statusCode = 404;
-        //   message = "Too many or too few";
-        //   return;
-        // }
-
-        console.log(data);
-
-        result = data;
-
-        return {
-          statusCode: statusCode,
-          body: JSON.stringify(result)
-        };
-
-        // return {
-        //   statusCode: 200,
-        //   body: JSON.stringify(data)
-        // };
-      }
+    try {
+      responseBody = await docClient.scan(params).promise();
+      statusCode = 200;
+    } catch (err) {
+      console.log(err);
     }
 
-    // switch (event.path) {
-    //   case "/login":
-    //     return {
-    //       statusCode: statusCode,
-    //       body: message
-    //     };
-    //     break;
-
-    //   case "/get-user":
-    //     return {
-    //       statusCode: 200,
-    //       body: "Get user"
-    //     };
-    //     break;
-
-    //   default:
-    //     console.error(
-    //       "Request to unknown end point received. Event details:",
-    //       JSON.stringify(event)
-    //     );
-    //     return {
-    //       statusCode: 404,
-    //       body: JSON.stringify(event)
-    //     };
-    // }
-
-    // return response;
+    return {
+      statusCode: statusCode,
+      body: responseBody
+    };
   } catch (e) {
     console.error(e);
     return {
